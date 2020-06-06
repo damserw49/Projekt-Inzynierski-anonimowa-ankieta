107e38231c30cfd9449a8b9a60f01e186a1f9049
damserw49 <65598379+damserw49@users.noreply.github.com>
2020-06-06 14:32:19 +0200

    Delete variables_1.js

--- dokumentacja/search/variables_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['else_35',['else',['../admin_8php.html#aa5cbd12da861cc12b78aef9881f90530',1,'else():&#160;admin.php'],['../authen__reg_8php.html#a07517bf4960de9edd85b1c1d8a4c5a2d',1,'else():&#160;authen_reg.php'],['../formularz_8php.html#aee331828e23436e9cd1780a515462afc',1,'else():&#160;formularz.php'],['../glowna_8php.html#acc804ecbb63483cab3d848c399e9fc05',1,'else():&#160;glowna.php'],['../index_8php.html#adea8b5b9e9c227b83a057eed525f7dce',1,'else():&#160;index.php']]],
-  ['exit_36',['exit',['../index_8php.html#a6733eb5f605d09eaede9845835d71c4e',1,'exit():&#160;index.php'],['../logout_8php.html#a6733eb5f605d09eaede9845835d71c4e',1,'exit():&#160;logout.php']]]
-];
