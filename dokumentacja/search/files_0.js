90bf2a4bf50ce5bdb4f3da3a59f32307bf77cde7
damserw49 <65598379+damserw49@users.noreply.github.com>
2020-06-06 14:30:01 +0200

    Delete files_0.js

--- dokumentacja/search/files_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['admin_2ephp_19',['admin.php',['../admin_8php.html',1,'']]],
-  ['authen_5flogin_2ephp_20',['authen_login.php',['../authen__login_8php.html',1,'']]],
-  ['authen_5freg_2ephp_21',['authen_reg.php',['../authen__reg_8php.html',1,'']]]
-];
